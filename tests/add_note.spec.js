753116274611434b58aac4527769261bb4492f4d
Prasanna-Kumar-1 <prasanna.kumar@outlook.com.au>
2023-08-13 22:00:23 +1000

    Enhanced the existing script

--- tests/add_note.spec.js
+++ tests/add_note.spec.js
@@ -2,5 +2,6 @@ import { test } from "@playwright/test";
 
 test("Adding a New Note", async ({ page }) => {
   await page.goto("https://practice.expandtesting.com/notes/app");
+  await page.getByRole("link", { name: "Login" }).click();
   await page.pause();
 });
