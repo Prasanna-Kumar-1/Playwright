fc9d292e891634275acc2f4c84559f3c5dad28b1
Prasanna-Kumar-1 <prasanna.kumar@outlook.com.au>
2023-08-14 22:04:20 +1000

    2nd Commit

--- tests/add_note.spec.js
+++ tests/add_note.spec.js
@@ -2,6 +2,13 @@ import { test } from "@playwright/test";
 
 test("Adding a New Note", async ({ page }) => {
   await page.goto("https://practice.expandtesting.com/notes/app");
-  await page.getByRole("link", { name: "Login" }).click();
-  await page.pause();
+  const loginBtn = page.getByRole("link", { name: "Login" });
+  await loginBtn.waitFor();
+  page.getByRole("link", { name: "Login" }).click();
+  const emailAddress = page.getByTestId("login-email");
+  await emailAddress.waitFor();
+  const pwd = page.getByTestId("login-password");
+  await emailAddress.waitFor();
+  const loginButton = page.getByTestId("login-submit");
+  await loginButton;
 });
